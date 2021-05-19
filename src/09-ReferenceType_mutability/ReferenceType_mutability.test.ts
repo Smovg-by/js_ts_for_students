3eb075beaac1112bff01ff96bc8c55c482607959
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-05-19 13:57:48 +0300

    lesson 9. Reference type, mutability of objects, arrays, primitives

---
+++ src/09-ReferenceType_mutability/ReferenceType_mutability.test.ts
@@ -0,0 +1,73 @@
+// Парадигма функционального программирования не допускает изменения исходных объектов.
+// Изменение объектов и их передача происходит по ссылке объекта. Нельзя просто присвоить значение объекта1 = оъект2 (так мы передаем ссылку reference type, соответственно все свойства будут меняться).
+
+test('OBJECT reference type test', () => {
+  type UserType = {
+    name: string
+    age: number
+  }
+
+  function increaseAge (u: UserType) {
+    u.age++
+  }
+
+  let user = {
+    name: 'Dimych',
+    age: 32
+  }
+
+  increaseAge(user)
+
+  expect(user.age).toBe(33)
+})
+
+// Изменение массивов происходит так же, передача  по ссылке. Чтобы не изменять исходный массив нужно его скопировать!
+
+test('ARRAY test', () => {
+  type UserType = {
+    name: string
+    age: number
+  }
+
+  let usersArray: Array<UserType> = [
+    {
+      name: 'Dimych',
+      age: 32
+    },
+
+    {
+      name: 'Mike',
+      age: 35
+    }
+  ]
+
+  let admins = usersArray
+
+  admins.push({
+    name: 'John',
+    age: 18
+  })
+
+  expect(usersArray.length).toBe(3)
+})
+
+//Примитивы НЕ передаются по ссылке!
+
+test('VALUE type test', () => {
+  let usersCount = 10
+
+  let adminsCount = usersCount
+
+  adminsCount++
+
+  expect(usersCount).toBe(10)
+  expect(adminsCount).toBe(11)
+})
+
+test('.sort() array test', () => {
+  let letters = ['b', 'c', 'a']
+
+  letters.sort()
+
+  expect(letters).toEqual(['a', 'b', 'c'])
+})
